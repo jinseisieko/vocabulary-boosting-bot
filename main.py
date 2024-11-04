112a73c0e12271a21830288f95f9fbe66f340e70
jinseisieko <144102580+jinseisieko@users.noreply.github.com>
2024-11-04 23:57:17 +0300

    evn technology is connected

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import logging
+import os
+
+from dotenv import load_dotenv
 
 logger = logging.getLogger(__name__)
 
 
 def main():
-    print('hello world!!!')
+    print(os.getenv("BOT_TOKEN"))
 
 
 if __name__ == "__main__":
+    load_dotenv()
     logging.basicConfig(
         filename='.log',
+        level=logging.NOTSET,
         format='%(asctime)s.%(msecs)03d [%(levelname)s] :%(name)s.%(funcName)s.%(lineno)d: %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S',
     )
