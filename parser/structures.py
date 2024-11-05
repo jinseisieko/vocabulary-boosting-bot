fcaf639fed99b77a1ffa00e3e7dde714cbfa2286
jinseisieko <144102580+jinseisieko@users.noreply.github.com>
2024-11-05 23:05:00 +0300

    correcting a flaw of classes

--- parser/structures.py
+++ parser/structures.py
@@ -2,19 +2,16 @@ from datetime import datetime
 
 
 class Data:
-    __slots__ = ("data",)
-
-    def __init__(self, data: dict):
-        super().__init__()
-        self.data: dict = data
-
-
-class SResponse(Data):
-    __slots__ = ("date_time",)
+    __slots__ = ("data", "date_time",)
 
     def __init__(self, data: dict):
         super().__init__(data)
+        self.data = data
         self.date_time = datetime.now()
+
+
+class SResponse(Data):
+    ...
 
 
 class SRequest:
