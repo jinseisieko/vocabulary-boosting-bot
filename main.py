19e9d875d6cdac0f8e83a4f250c78f30ff0869f2
jinseisieko <jinseisieko@gmail.com>
2024-11-08 21:28:52 +0300

    create package tools, now it's nothing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import logging
 import os
 
+import requests
 from dotenv import load_dotenv
 
 from parser.structures import SMRequest
@@ -9,10 +10,8 @@ logger = logging.getLogger(__name__)
 
 
 def main():
-    logger.info("123")
     print(os.getenv("BOT_TOKEN"))
-    for _ in range(1):
-        parser.get_data_from_multitran(SMRequest("can"))
+    parser.get_data_from_multitran(SMRequest("can"))
 
 
 if __name__ == "__main__":
